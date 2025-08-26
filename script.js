3f2cc0706ecfd9e5632805835be25d1d6f21d42c
KallamSamad <kallamsamad@gmail.com>
2025-08-26 11:20:59 +0100

    added game logic by asking user for input and used this input as an argument to place X into the index of the gameboard

--- script.js
+++ script.js
@@ -1,8 +1,37 @@
-let gameBoardObject = {gameBoard:[]}
+let gameBoardObject = {
+    gameBoard:
+    ["", "", "",
+    "",  "", "",
+    "",  "", ""
 
+    ]}
+let playerObject=
+{
+    player1Score:0,
+    player2Score: 0,
+    player1Marker: "X",
+    player2Marker: "O"
 
-for (let i=0;i<=9; i++){
-    gameBoardObject.gameBoard.push(i)
 }
 
-console.log(gameBoardObject.gameBoard)
+let gameFlow={
+    turn: "player1",
+    gameOver: "False",
+    winner: "winner"
+
+
+}
+
+let position=0
+function game(position){
+    position=prompt("Choose a place 1-9")
+    gameBoardObject.gameBoard[position]=playerObject.player1Marker
+    console.log(gameBoardObject.gameBoard)
+
+
+}
+
+game(position)
+
+
+
